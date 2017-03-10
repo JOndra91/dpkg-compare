8a30df983ccf1a5c37d08dfaf30bf2b28e29f792
Ondřej Janošík <j.ondra14@gmail.com>
2017-03-10 20:54:06 +0100

    Command line arguments for filtering

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # dpkg-compare
+
+Utility for comparing output of 'dpkg --list' command.
+
+## Usage
+
+```
+dpkg-compare {[-r|--reference] REFERENCE_FILE} {[-o|--other] OTHER_FILE}
+             [--only-extra|--only-missing|--only-mismatch] [--only-installed]
+dpkg-compare {-h|--help}
+
+  -h                 --help                      Print this help.
+  -r REFERENCE_FILE  --reference=REFERENCE_FILE  Path to file that's result of 'dpkg --list' command.
+  -o OTHER_FILE      --other=OTHER_FILE          Path to file that's result of 'dpkg --list' command.
+                     --only-extra                Print only packages found in REFERENCE_FILE but not in OTHER_FILE.
+                     --only-missing              Print only packages found in OTHER_FILE but not in REFERENCE_FILE.
+                     --only-mismatch             Print only packages found in both files but not with same version/architecture/status.
+                     --only-installed            Print only packages with 'installed' status.
+```
--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,41 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedLabels #-}
 
 module Main where
 
 import Control.Monad
 import Data.Char
-import Data.Monoid
-import System.Environment
-import Text.ParserCombinators.ReadP
-import Debug.Trace
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import qualified Data.Map.Merge.Strict as Map
+import Data.Monoid
 import qualified Data.Set as Set
+import System.Console.GetOpt
+import System.Environment
+import System.Exit
+import Text.ParserCombinators.ReadP
+
+data ProgOpts = ProgOpts
+    { optReferenceFile :: String
+    , optOtherFile :: String
+    , optFilter :: OutputFilter
+    , optStatus :: OutputStatus
+    , optHelp :: Bool
+    }
+  deriving (Show)
+
+data OutputFilter
+    = FilterAll
+    | FilterOnlyExtra
+    | FilterOnlyMissing
+    | FilterOnlyMismatch
+  deriving (Show)
+
+data OutputStatus
+    = StatusAll
+    | StatusOnlyInstalled
+  deriving (Show)
 
 data Status
     = None
@@ -47,20 +70,104 @@ data Difference
   deriving (Show)
 
 main :: IO ()
-main = getArgs >>= \case
-    [f1, f2] -> do
-        referenceMap <- mkPkgMap . parseFile <$> readFile f1
-        otherMap <- mkPkgMap . parseFile <$> readFile f2
-        let diff = Map.merge
-              (Map.mapMissing $ (Extra .) . (flip const))
-              (Map.mapMissing $ (Missing .) . (flip const))
-              (Map.zipWithMatched matchVersion)
-              referenceMap otherMap
-        putStrLn . unlines $ show <$> Map.elems diff
+main = getArgs >>= \args -> case getOpt Permute argsDef args of
+    (opts, fs, []) -> doStuff . applyNonOpts fs $ (foldl (.) id opts) defOpts
+    (_, _, err:_) -> die $ err <> usage
   where
+    defOpts = ProgOpts "" "" FilterAll StatusAll False
+
+    applyNonOpts fs opts@ProgOpts{..} = case fs of
+        [f1,f2] -> opts
+            { optReferenceFile = f1
+            , optOtherFile = f2
+            }
+        _ -> error "Invalid arguments."
+
+usage :: String
+usage = usageInfo str argsDef
+  where
+    str = unlines
+        [ "Utility for comparing output of 'dpkg --list' command."
+        , ""
+        , "Usage:"
+        , "dpkg-compare {[-r|--reference] REFERENCE_FILE} {[-o|--other] OTHER_FILE}"
+        , "             [--only-extra|--only-missing|--only-mismatch] [--only-installed]"
+        , "dpkg-compare {-h|--help}"
+        ]
+
+argsDef :: [OptDescr (ProgOpts -> ProgOpts)]
+argsDef =
+    [ Option ['h'] ["help"]
+        (NoArg (\opts -> opts { optHelp = True }))
+        "Print this help."
+    , Option ['r'] ["reference"]
+        (ReqArg (\s opts -> opts { optReferenceFile = s }) "REFERENCE_FILE")
+        "Path to file that's result of 'dpkg --list' command."
+    , Option ['o'] ["other"]
+        (ReqArg (\s opts -> opts { optOtherFile = s }) "OTHER_FILE")
+        "Path to file that's result of 'dpkg --list' command."
+    , Option [] ["only-extra"]
+        (NoArg (\opts -> opts { optFilter = FilterOnlyExtra }))
+        "Print only packages found in REFERENCE_FILE but not in OTHER_FILE."
+    , Option [] ["only-missing"]
+        (NoArg (\opts -> opts { optFilter = FilterOnlyMissing }))
+        "Print only packages found in OTHER_FILE but not in REFERENCE_FILE."
+    , Option [] ["only-mismatch"]
+        (NoArg (\opts -> opts { optFilter = FilterOnlyMismatch }))
+        "Print only packages found in both files but not with same version/architecture/status."
+    , Option [] ["only-installed"]
+        (NoArg (\opts -> opts { optStatus = StatusOnlyInstalled }))
+        "Print only packages with 'installed' status."
+    ]
+
+doStuff :: ProgOpts -> IO ()
+doStuff ProgOpts{..}
+  | optHelp = putStrLn usage
+  | otherwise = do
+    when (null optReferenceFile) $ die "REFERENCE_FILE not specified."
+    when (null optOtherFile) $ die "OTHER_FILE not specified."
+    referenceMap <- loadFile optReferenceFile
+    otherMap <- loadFile optOtherFile
+    let diff = Map.merge
+          (Map.mapMissing $ (Extra .) . (flip const))
+          (Map.mapMissing $ (Missing .) . (flip const))
+          (Map.zipWithMatched matchVersion)
+          referenceMap otherMap
+    putStrLn . unlines . fmap show . pkgFilter $ Map.elems diff
+  where
+    loadFile = fmap (mkPkgMap . pkgStatus . parseFile) . readFile
+
     matchVersion _ left right
         | left == right = Same left
         | otherwise = Mismatch left right
+
+    pkgStatus :: [Package] -> [Package]
+    pkgStatus = case optStatus of
+        StatusAll -> id
+        StatusOnlyInstalled -> filter isInstalled
+
+    isInstalled = \case
+        Package _ _ _ Installed -> True
+        _ -> False
+
+    pkgFilter :: [Difference] -> [Difference]
+    pkgFilter = case optFilter of
+        FilterAll -> id
+        FilterOnlyExtra -> filter isExtra
+        FilterOnlyMissing -> filter isMissing
+        FilterOnlyMismatch -> filter isMismatch
+
+    isMissing = \case
+        Missing _ -> True
+        _ -> False
+
+    isExtra = \case
+        Extra _ -> True
+        _ -> False
+
+    isMismatch = \case
+        Mismatch _ _ -> True
+        _ -> False
 
 parseFile :: String -> [Package]
 parseFile = fmap parsePkg . drop 5 . lines
